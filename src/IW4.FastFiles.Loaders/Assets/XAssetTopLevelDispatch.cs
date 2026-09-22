3c176793424f891123ce1972e984def6292a3374
Jacob Schroeder <87840838+jacob-schroeder@users.noreply.github.com>
2026-09-22 12:35:07 -0400

    Consolidate XAsset dispatch routes

--- src/IW4.FastFiles.Loaders/Assets/XAssetTopLevelDispatch.cs
+++ src/IW4.FastFiles.Loaders/Assets/XAssetTopLevelDispatch.cs
@@ -8,46 +8,6 @@ namespace IW4.FastFiles.Loaders.Assets;
 /// </summary>
 public static class XAssetTopLevelDispatch
 {
-    private static readonly HashSet<XAssetType> PointerWrappedTypes =
-    [
-        XAssetType.PhysPreset,
-        XAssetType.PixelShader,
-        XAssetType.VertexShader,
-        XAssetType.Techset,
-        XAssetType.Image,
-        XAssetType.Material,
-        XAssetType.MenuFile,
-        XAssetType.Menu,
-        XAssetType.StringTable,
-        XAssetType.StructuredDataDef,
-        XAssetType.RawFile,
-        XAssetType.Localize,
-        XAssetType.Sound,
-        XAssetType.SndCurve,
-        XAssetType.LoadedSound,
-        XAssetType.Fx,
-        XAssetType.ImpactFx,
-        XAssetType.XAnim,
-        XAssetType.XModelSurfs,
-        XAssetType.XModel,
-        XAssetType.PhysCollmap,
-        XAssetType.Font,
-        XAssetType.Vehicle,
-        XAssetType.LightDef,
-        XAssetType.ColMapSp,
-        XAssetType.ColMapMp,
-        XAssetType.MapEnts,
-        XAssetType.ComMap,
-        XAssetType.FxMap,
-        XAssetType.GfxMap,
-        XAssetType.GameMapSp,
-        XAssetType.GameMapMp,
-        XAssetType.AddonMapEnts,
-        XAssetType.Weapon,
-        XAssetType.LeaderboardDef,
-        XAssetType.Tracer
-    ];
-
     public static XAssetTopLevelDispatchKind Classify(XAssetType assetType)
     {
         // Native no-op types preserve their opaque XAssetHeader words without
@@ -57,7 +17,7 @@ public static class XAssetTopLevelDispatch
             return XAssetTopLevelDispatchKind.NativeNoOp;
         }
 
-        return PointerWrappedTypes.Contains(assetType)
+        return XAssetDispatcher.HasPointerWrappedRoute(assetType)
             ? XAssetTopLevelDispatchKind.PointerWrapper
             : XAssetTopLevelDispatchKind.Unsupported;
     }
